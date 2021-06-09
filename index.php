6a6529c61266f759c84d0796dfb19a7ccc209a2b
Lazzytchik <GitLazzytchik@users.noreply.github.com>
2021-06-09 15:36:23 +0300

    Charset settings
    .htaccess added
    html pattern included in index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php echo 'Hello world'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>News</title>
+    </head>
+    
+    <body>
+        asdasdasd фываываыва
+    </body>
+</html>
