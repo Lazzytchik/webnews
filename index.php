1d8688f8a0e65531af539d1a681b561925ded470
Lazzytchik <GitLazzytchik@users.noreply.github.com>
2021-06-10 00:38:50 +0300

    Query upgrade
    - added query in index.php
    - add navigation bar with extra levels
    - style sheet
    - upgraded .htaccess file to make custom links

--- index.php
+++ index.php
@@ -10,12 +10,35 @@
     
     <body>
         
+        <nav>
+            <ul class="topmenu">
+                <li><a href="" class="active">Главная<span class="fa fa-angle-down"></span></a>
+                    <ul class="submenu">
+                        <li><a href="">меню второго уровня</a></li>
+                        <li><a href="">меню второго уровня<span class="fa fa-angle-down"></span></a>
+                            <ul class="submenu">
+                                <li><a href="">меню третьего уровня</a></li>
+                                <li><a href="">меню третьего уровня</a></li>
+                                <li><a href="">меню третьего уровня</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="">меню второго уровня</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Компания</a></li>
+                <li><a href="">Блог</a></li>
+                <li><a href="">Контакты</a></li>
+            </ul>
+        </nav>
+        
         <?php
         
             require 'dbconfig.php';
             
-            $get_news = 'SELECT * FROM news ORDER by post_date DESC';
-            $query = $pdo->query($get_news);
+            $get_news = "SELECT * FROM news JOIN categorized_news cn on news.id = cn.news_id WHERE group_name = :group and cat_name = :category ORDER by post_date DESC";
+            //echo $get_news;
+            $query = $pdo->prepare($get_news);
+            $query->execute(['group' => 'php', 'category' => 'Обучение']);
             while($row = $query->fetch(PDO::FETCH_OBJ)){
                 require 'news_form.php';
             }
