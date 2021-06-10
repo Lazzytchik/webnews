8b140ea4aa3bedb051485eb0d04281270dda3e5f
Lazzytchik <GitLazzytchik@users.noreply.github.com>
2021-06-10 22:56:09 +0300

    final build

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>News</title>
-        <link href="css/style.css" rel="stylesheet" type="text/css">
-    </head>
-    
-    <body>
-        
-        <?
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>News</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+    <?
         
             require 'dbconfig.php';
             $db = new NewsDB('localhost', 'webnews');
@@ -21,13 +22,13 @@
         
             require 'mainmenu.php';
         
-        ?>
-        
-        <div id='main'>
-            <h1> Добро пожаловать на тестовый новостной портал!</h1>
-            <p>На этом небольшом новостном портале вы можете найти новости в сфере веб-технологий на различные темы.</p>
-        </div>
+    ?>
 
-        
-    </body>
+    <div id='main'>
+        <h1> Добро пожаловать на тестовый новостной портал!</h1>
+        <p>На этом небольшом новостном портале вы можете найти новости в сфере веб-технологий на различные темы.</p>
+    </div>
+    
+</body>
+
 </html>
